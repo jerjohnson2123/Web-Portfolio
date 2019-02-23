3600ccb4fe35d4403289d2210d6b2fcf49e0c1d5
jerjohnson2123 <jer.johnson2123@gmail.com>
2019-02-23 00:56:59 -0700

    Updating code
    Adding code that is used for the form on the 'Web' page.

--- docs/Template1_Edit.js
+++ docs/Template1_Edit.js
@@ -5,3 +5,9 @@ function openNav() {
 function closeNav() {
 	document.getElementById("sideNav").style.width = "0";
 }
+
+function display() {
+	document.getElementById("exForm").innerHTML ="Thank you " + document.exForm.firstname.value +
+	" " + document.exForm.lastname.value + " you have select " + document.exForm.BigFeburaryGames.value + 
+	" for your free monthly game." + " A confirmation email will be sent to " + document.exForm.email.value + ".";
+}
