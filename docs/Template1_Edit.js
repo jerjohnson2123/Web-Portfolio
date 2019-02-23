c0c92154fb495f4b15497ddb545e11d2a36b8dde
jerjohnson2123 <jer.johnson2123@gmail.com>
2019-02-23 03:59:47 -0700

    Add files via upload

--- docs/Template1_Edit.js
+++ docs/Template1_Edit.js
@@ -11,3 +11,25 @@ function display() {
 	" " + document.exForm.lastname.value + " you have select " + document.exForm.BigFeburaryGames.value + 
 	" for your free monthly game." + " A confirmation email will be sent to " + document.exForm.email.value + ".";
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
